36777d00ba587a49c66af96518cb4a0a9ec399bb
dhr7va <138505718+dhr7va@users.noreply.github.com>
2024-01-30 21:55:47 +0530

    Create LCS of three strings.py
    Given 3 strings A, B and C, the task is to find the length of the longest sub-sequence that is common in all the three given strings.

---
+++ LCS of three strings.py
@@ -0,0 +1,35 @@
+#User function Template for python3
+
+class Solution:
+
+    def LCSof3(self,A,B,C,n1,n2,n3):
+        dp = [[0] * (n2 + 1) for _ in range(n1 + 1)]
+
+        for i in range(1, n1 + 1):
+            for j in range(1, n2 + 1):
+                for k in range(1, n3 + 1):
+                    if A[i - 1] == B[j - 1] == C[k - 1]:
+                        dp[i][j] = dp[i - 1][j - 1] + 1
+                    else:
+                        dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+        return dp[n1][n2]
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+
+    t = int(input())
+
+    for _ in range(t):
+        n1,n2,n3 = map(int,input().split())
+        A,B,C = input().split()
+
+        solObj = Solution()
+
+        ans = solObj.LCSof3(A,B,C,n1,n2,n3)
+
+        print(ans)
+# } Driver Code Ends
