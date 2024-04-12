767cee9044a5dcaa2b12f2d9c391d88f647ebbeb
Dhruva Kumar <138505718+dhr7va@users.noreply.github.com>
2024-04-12 22:03:16 +0530

    Update 06-04-2024.py
    Count ways to N'th Stair
    
    There are n stairs, and a person standing at the bottom wants to reach the top. The person can climb either 1 stair or 2 stairs at a time. Count the number of ways, the person can reach the top (order does not matter).

--- April/06-04-2024.py
+++ April/06-04-2024.py
@@ -1,70 +1,29 @@
 #User function Template for python3
 
-# arr[] : int input array of integers
-# k : the quadruple sum required
+
 class Solution:
-    def fourSum(self, arr, k):
-        # code here
-        n = len(arr)
-        arr.sort() 
-        res = []   
-        
-        for i in range(n - 3): 
-            if i > 0 and arr[i] == arr[i - 1]: 
-                continue
-            
-            for j in range(i + 1, n - 2): 
-                if j > i + 1 and arr[j] == arr[j - 1]:  
-                    continue
-                
-                left = j + 1 
-                right = n - 1
-                
-                while left < right: 
-                    curr_sum = arr[i] + arr[j] + arr[left] + arr[right]
-                    
-                    if curr_sum == k:
-                        res.append([arr[i], arr[j], arr[left], arr[right]])
-                        left += 1
-                        right -= 1
-                        
-                        while left < right and arr[left] == arr[left - 1]:
-                            left += 1
-                        while left < right and arr[right] == arr[right + 1]:
-                            right -= 1
-                    elif curr_sum < k:
-                        left += 1
-                    else:
-                        right -= 1
-        
-        return res
+
+    #Function to count number of ways to reach the nth stair
+    #when order does not matter.
+    def countWays(self, n: int) -> int:
+        return n // 2 + 1
+
 
 
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
+import atexit
+import io
+import sys
 
+# Contributed by : Nagendra Jha
 
-#Main
-if __name__=='__main__':
-    t = int(input())
-    while t:
-        t-=1
-        n, k=map(int,input().split())
-        # print(n, k)
-        a=list(map(int,input().strip().split()))[:n]
-        # print(a)
-        ob=Solution()
-        ans=ob.fourSum(a, k)
-        # print(ans)
-        for v in ans:
-            for u in v:
-                print(u, end=" ")
-            print("$", end="")
-        if(len(ans)==0):
-            print(-1, end="")
-        print()
-        
-        
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        n = int(input())
+        ob = Solution()
+        print(ob.countWays(n))
 
 # } Driver Code Ends
