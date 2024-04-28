a996a21463f261f814b3079fec43fb18c0b7d6ec
Dhruva Kumar <138505718+dhr7va@users.noreply.github.com>
2024-04-28 13:02:45 +0530

    Create 28-04-2024.py
    Delete Middle of Linked List
    
    Given a singly linked list, delete middle of the linked list. For example, if given linked list is 1->2->3->4->5 then linked list should be modified to 1->2->4->5.
    If there are even nodes, then there would be two middle nodes, we need to delete the second middle element. For example, if given linked list is 1->2->3->4->5->6 then it should be modified to 1->2->3->5->6.
    If the input linked list has single node, then it should return NULL.

---
+++ April/28-04-2024.py
@@ -0,0 +1,83 @@
+#User function Template for python3
+
+'''
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+'''
+class Solution:
+    def deleteMid(self, head):
+        """
+        head: head of given linkedList
+        return: head of resultant list
+        """
+        if not head or not head.next:
+            return None
+        
+        length = 0
+        current = head
+        while current:
+            length += 1
+            current = current.next
+        
+
+        middle_position = length // 2
+        
+        current = head
+        for _ in range(middle_position - 1):
+            current = current.next
+        
+
+        current.next = current.next.next
+        
+        return head
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Llist:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, data, tail):
+        node = Node(data)
+
+        if not self.head:
+            self.head = node
+            return node
+
+        tail.next = node
+        return node
+
+
+def printList(head):
+    while head:
+        print(head.data, end=' ')
+        head = head.next
+    print()
+
+if __name__ == '__main__':
+    t = int(input())
+
+    for tcs in range(t):
+        n=int(input())
+        arr1 = [int(x) for x in input().split()]
+        ll = Llist()
+        tail = None
+        for nodeData in arr1:
+            tail = ll.insert(nodeData, tail)
+        obj = Solution();
+        res=obj.deleteMid(ll.head)
+        printList(res)
+# } Driver Code Ends
