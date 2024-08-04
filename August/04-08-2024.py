cb2ed9ff2becb6c3b15239dbeb0eba5db5ae8a69
Dhruva Kumar <138505718+dhr7va@users.noreply.github.com>
2024-08-04 18:54:24 +0530

    Create 04-08-2024.py
    N meetings in one room
    Difficulty: EasyAccuracy: 45.3%Submissions: 256K+Points: 2
    
    You are given timings of n meetings in the form of (start[i], end[i]) where start[i] is the start time of meeting i and end[i] is the finish time of meeting i. Return the maximum number of meetings that can be accommodated in a single meeting room, when only one meeting can be held in the meeting room at a particular time. 
    
    Note: The start time of one chosen meeting can't be equal to the end time of the other chosen meeting.
    
    Examples :
    
    Input: n = 6, start[] = [1, 3, 0, 5, 8, 5], end[] =  [2, 4, 6, 7, 9, 9]
    Output: 4
    Explanation: Maximum four meetings can be held with given start and end timings. The meetings are - (1, 2), (3, 4), (5,7) and (8,9)
    
    Input: n = 3, start[] = [10, 12, 20], end[] = [20, 25, 30]
    Output: 1
    Explanation: Only one meetings can be held with given start and end timings.
    
    Expected Time Complexity: O(n*logn)
    Expected Auxilliary Space: O(n)
    
    Constraints:
    1 ≤ n ≤ 10^5
    0 ≤ start[i] < end[i] ≤ 10^6

---
+++ August/04-08-2024.py
@@ -0,0 +1,38 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to find the maximum number of meetings that can
+    #be performed in a meeting room.
+    def maximumMeetings(self,n,start,end):
+        # code here
+        meetings = sorted(zip(start, end), key=lambda x: x[1])
+        
+        count = 0
+        last_end_time = 0
+        
+        for s, e in meetings:
+            if s > last_end_time:  
+                count += 1         
+                last_end_time = e   
+        
+        return count
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+#Contributed by : Nagendra Jha
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        n = int(input())
+        start = list(map(int,input().strip().split()))
+        end = list(map(int,input().strip().split()))
+        ob=Solution()
+        print(ob.maximumMeetings(n,start,end))
+# } Driver Code Ends
