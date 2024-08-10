e18d3ea056cc127e7ca4596e0608a1b6c0a2c549
Dhruva Kumar <138505718+dhr7va@users.noreply.github.com>
2024-08-10 19:25:13 +0530

    Create 10-08-2024.py
    Rotate a Linked List
    Difficulty: MediumAccuracy: 39.95%Submissions: 234K+Points: 4
    
    Given the head of a singly linked list, the task is to rotate the linked list anti-clockwise by k nodes, i.e., left-shift the linked list by k nodes, where k is a given positive integer smaller than or equal to length of the linked list.
    
    Examples:
    
    Input: linkedlist: 2->4->7->8->9 , k = 3
    Output: 8->9->2->4->7
    Explanation:
    Rotate 1: 4 -> 7 -> 8 -> 9 -> 2
    Rotate 2: 7 -> 8 -> 9 -> 2 -> 4
    Rotate 3: 8 -> 9 -> 2 -> 4 -> 7
    
    Input: linkedlist: 1->2->3->4->5->6->7->8 , k = 4
    Output: 5->6->7->8->1->2->3->4
    
    Expected Time Complexity: O(n)
    Expected Auxiliary Space: O(1)
    
    Constraints:
    1 <= number of nodes <= 103
    1 <= node -> data <= 104
    1 <= k <= number of nodes

---
+++ August/10-08-2024.py
@@ -0,0 +1,84 @@
+# Your task is to complete this function
+
+'''
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+'''
+
+
+class Solution:
+    
+    #Function to rotate a linked list.
+    def ro(self,h,t):
+        he=h
+        h=h.next
+        he.next=None
+        t.next=he
+        return h,t.next
+        
+    def rotate(self, head, k):
+        # code here
+        n=1
+        t=head
+        while t.next:
+            t=t.next
+            n+=1
+        k=k%n
+        for i in range(k):
+            head,t=self.ro(head,t)
+        return head
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+# Define the Node class for the linked list
+class Node:
+
+    def __init__(self, x):
+        self.data = x
+        self.next = None
+
+
+# Function to print the linked list
+def printList(node):
+    while node:
+        print(node.data, end=" ")
+        node = node.next
+    print()
+
+
+#Position this line where user code will be pasted.
+
+# Main function
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.read
+    data = input().splitlines()
+
+    t = int(data[0].strip())
+    idx = 1
+
+    while t > 0:
+        arr = list(map(int, data[idx].strip().split()))
+
+        head = None
+        if arr:
+            head = Node(arr[0])
+            tail = head
+            for num in arr[1:]:
+                tail.next = Node(num)
+                tail = tail.next
+
+        k = int(data[idx + 1].strip())
+        idx += 2
+        head = Solution().rotate(head, k)
+        printList(head)
+        t -= 1
+
+# } Driver Code Ends
