2445bfb277439ff1bc9585de1abfe31508b93f4d
Dhruva Kumar <138505718+dhr7va@users.noreply.github.com>
2024-08-11 13:29:32 +0530

    Create 11-08-2024.py
    Back to Explore Page
    Job Sequencing Problem
    Difficulty: MediumAccuracy: 34.51%Submissions: 230K+Points: 4
    
    Given a set of n jobs where each jobi has a deadline and profit associated with it.
    
    Each job takes 1 unit of time to complete and only one job can be scheduled at a time. We earn the profit associated with a job if and only if the job is completed by its deadline.
    
    Find the number of jobs done and the maximum profit.
    
    Note: Jobs will be given in the form (Jobid, Deadline, Profit) associated with that Job. Deadline of the job is the time on or before which job needs to be completed to earn the profit.
    
    Examples :
    
    Input: Jobs = [[1,4,20],[2,1,1],[3,1,40],[4,1,30]]
    Output: 2 60
    Explanation: Job1 and Job3 can be done with maximum profit of 60 (20+40).
    
    Input: Jobs = [[1,2,100],[2,1,19],[3,2,27],[4,1,25],[5,1,15]]
    Output: 2 127
    Explanation: 2 jobs can be done with maximum profit of 127 (100+27).
    
    Expected Time Complexity: O(nlogn)
    Expected Auxilliary Space: O(n)
    
    Constraints:
    1 <= n <= 10^5
    1 <= Deadline,id <= n
    1 <= Profit <= 500

---
+++ August/11-08-2024.py
@@ -0,0 +1,81 @@
+#User function Template for python3
+'''
+class Job:
+    
+    # Job class which stores profit and deadline.
+    
+    def __init__(self,profit=0,deadline=0):
+        self.profit = profit
+        self.deadline = deadline
+        self.id = 0
+'''        
+
+class Solution:
+    
+    #Function to find the maximum profit and the number of jobs done.
+    def JobScheduling(self,Jobs,n):
+        
+        # code here
+        Jobs.sort(key=lambda x: x.profit, reverse=True)
+        
+        max_deadline = max(job.deadline for job in Jobs)
+        
+        parent = list(range(max_deadline + 1))
+        
+        def find(x):
+            if parent[x] == x:
+                return x
+            parent[x] = find(parent[x])
+            return parent[x]
+        
+        def union(x, y):
+            parent[find(x)] = find(y)
+        
+        count_jobs = 0
+        max_profit = 0
+        
+        for job in Jobs:
+            available_slot = find(min(max_deadline, job.deadline))
+            if available_slot > 0:
+                union(available_slot, available_slot - 1)
+                count_jobs += 1
+                max_profit += job.profit
+        
+        return count_jobs, max_profit
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+
+#Contributed by : Nagendra Jha
+class Job:
+    '''
+    Job class which stores profit and deadline.
+    '''
+
+    def __init__(self, profit=0, deadline=0):
+        self.profit = profit
+        self.deadline = deadline
+        self.id = 0
+
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        n = int(input())
+        info = list(map(int, input().strip().split()))
+        Jobs = [Job() for i in range(n)]
+        for i in range(n):
+            Jobs[i].id = info[3 * i]
+            Jobs[i].deadline = info[3 * i + 1]
+            Jobs[i].profit = info[3 * i + 2]
+        ob = Solution()
+        res = ob.JobScheduling(Jobs, n)
+        print(res[0], end=" ")
+        print(res[1])
+
+# } Driver Code Ends
