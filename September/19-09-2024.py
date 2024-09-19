f73b9b94a4bc2a2ac0a5d3e9bec9b40df216e5d4
Dhruva Kumar <138505718+dhr7va@users.noreply.github.com>
2024-09-19 19:33:52 +0530

    Create 19-09-2024.py
    Reverse Words
    Difficulty: EasyAccuracy: 56.08%Submissions: 332K+Points: 2
    
    Given a String str, reverse the string without reversing its individual words. Words are separated by dots.
    
    Note: The last character has not been '.'. 
    
    Examples :
    
    Input: str = i.like.this.program.very.much
    Output: much.very.program.this.like.i
    Explanation: After reversing the whole string(not individual words), the input string becomes much.very.program.this.like.i
    
    Input: str = pqr.mno
    Output: mno.pqr
    Explanation: After reversing the whole string , the input string becomes mno.pqr
    
    Expected Time Complexity: O(|str|)
    Expected Auxiliary Space: O(|str|)
    
    Constraints:
    1 <= |str| <= 10^5

---
+++ September/19-09-2024.py
@@ -0,0 +1,25 @@
+# User function Template for python3
+
+class Solution:
+    
+    #Function to reverse words in a given string.
+    def reverseWords(self,str):
+        # code here 
+        words = str.split('.')
+        
+        words.reverse()
+        
+        return '.'.join(words)
+
+#{ 
+ # Driver Code Starts
+# Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        s = str(input())
+        obj = Solution()
+        print(obj.reverseWords(s))
+
+# } Driver Code Ends
