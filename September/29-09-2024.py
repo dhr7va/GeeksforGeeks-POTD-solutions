e1fc3abec9037da6ebb8fef5bc07f418ba89d951
Dhruva Kumar <138505718+dhr7va@users.noreply.github.com>
2024-09-29 18:49:31 +0530

    Create 29-09-2024.py
    Back to Explore Page
    Total count
    Difficulty: EasyAccuracy: 48.74%Submissions: 45K+Points: 2
    
    You are given an array arr[] of positive integers and a threshold value k. For each element in the array, divide it into the minimum number of small integers such that each divided integer is less than or equal to k. Compute the total number of these integer across all elements of the array.
    
    Examples:
    
    Input: k = 3, arr[] = [5, 8, 10, 13]
    Output: 14
    Explanation: Each number can be expressed as sum of different numbers less than or equal to k as 5 (3 + 2), 8 (3 + 3 + 2), 10 (3 + 3 + 3 + 1), 13 (3 + 3 + 3 + 3 + 1). So, the sum of count of each element is (2+3+4+5)=14.
    
    Input: k = 4, arr[] = [10, 2, 3, 4, 7]
    Output: 8
    Explanation: Each number can be expressed as sum of different numbers less than or equal to k as 10 (4 + 4 + 2), 2 (2), 3 (3), 4 (4) and 7 (4 + 3).So, the sum of count of each element is (3 + 1 + 1 + 1 + 2) = 8.
    
    Expected Time Complexity: O(n)
    Expected Auxiliary Space: O(1)
    
    Constraints:
    1 ≤ arr.size() ≤ 10^5
    0 ≤ arr[i] ≤ 10^5
    1 ≤ k ≤ 10^5

---
+++ September/29-09-2024.py
@@ -0,0 +1,32 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+
+class Solution:
+    def totalCount(self, k, arr):
+        # code here
+        total = 0
+        for x in arr:
+            total += (x + k - 1) // k  
+        return total
+        
+
+#{ 
+ # Driver Code Starts.
+#Initial Template for Python 3
+
+if __name__ == "__main__":
+    t = int(input())
+    while t > 0:
+        k= int(input())
+        arr = list(map(int, input().split()))
+        ob = Solution()
+        res = ob.totalCount(k,arr)
+        print(res)
+        t -= 1
+
+
+# } Driver Code Ends
