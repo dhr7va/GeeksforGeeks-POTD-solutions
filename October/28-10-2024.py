42e2d8eceaa71d012116d417fbfe9000dd60dcd9
Dhruva Kumar <138505718+dhr7va@users.noreply.github.com>
2024-10-28 20:20:52 +0530

    Create 28-10-2024.py
    Remove duplicates in array
    Difficulty: EasyAccuracy: 35.86%Submissions: 49K+Points: 2
    
    Given an array arr consisting of positive integer numbers, remove all duplicate numbers.
    
    Example:
    
    Input: arr[] = [2, 2, 3, 3, 7, 5] 
    Output: [2, 3, 7, 5]
    Explanation: After removing the duplicates 2 and 3 we get 2 3 7 5.
    
    Input: arr[] = [2, 2, 5, 5, 7, 7] 
    Output: [2, 5, 7]
    
    Input: arr[] = [8, 7] 
    Output: [8, 7]
    
    Constraints:
    1<= arr.size() <=10^6
    2<= arr[i] <=100

---
+++ October/28-10-2024.py
@@ -0,0 +1,27 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+class Solution:
+    def removeDuplicates(self, arr):
+        # code here 
+        unique_elements = list(dict.fromkeys(arr))
+        return unique_elements
+
+#{ 
+ # Driver Code Starts.
+#Initial Template for Python 3
+
+if __name__ == "__main__":
+    t = int(input())
+    while t > 0:
+        arr = list(map(int, input().split()))
+        ob = Solution()
+        ans = ob.removeDuplicates(arr)
+        print(*ans)
+        print("~")
+        t -= 1
+
+
+# } Driver Code Ends
