d73b5555f40492a453fd830de3e7a64a5693c33b
Dhruva Kumar <138505718+dhr7va@users.noreply.github.com>
2024-11-19 08:48:24 +0530

    Create 18-11-2024.py
    Rotate Array
    Difficulty: MediumAccuracy: 37.06%Submissions: 365K+Points: 4
    
    Given an unsorted array arr[]. Rotate the array to the left (counter-clockwise direction) by d steps, where d is a positive integer. Do the mentioned change in the array in place.
    
    Note: Consider the array as circular.
    
    Examples :
    
    Input: arr[] = [1, 2, 3, 4, 5], d = 2
    Output: [3, 4, 5, 1, 2]
    Explanation: when rotated by 2 elements, it becomes 3 4 5 1 2.
    
    Input: arr[] = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20], d = 3
    Output: [8, 10, 12, 14, 16, 18, 20, 2, 4, 6]
    Explanation: when rotated by 3 elements, it becomes 8 10 12 14 16 18 20 2 4 6.
    
    Input: arr[] = [7, 3, 9, 1], d = 9
    Output: [3, 9, 1, 7]
    Explanation: when we rotate 9 times, we'll get 3 9 1 7 as resultant array.
    
    Constraints:
    1 <= arr.size(), d <= 10^5
    0 <= arr[i] <= 10^5

---
+++ November/18-11-2024.py
@@ -0,0 +1,47 @@
+#User function Template for python3
+
+class Solution:
+    def rotateArr(self, arr, d):
+        n = len(arr)
+        d %= n  
+        self.reverse(arr, 0, d - 1)
+        self.reverse(arr, d, n - 1)
+        self.reverse(arr, 0, n - 1)
+
+    def reverse(self, arr, start, end):
+        while start < end:
+            arr[start], arr[end] = arr[end], arr[start]
+            start += 1
+            end -= 1
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+def main():
+    T = int(input())
+
+    while (T > 0):
+        A = [int(x) for x in input().strip().split()]
+        nd = [int(x) for x in input().strip().split()]
+        D = nd[0]
+        ob = Solution()
+        ob.rotateArr(A, D)
+
+        for i in A:
+            print(i, end=" ")
+
+        print()
+
+        T -= 1
+
+        print("~")
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
