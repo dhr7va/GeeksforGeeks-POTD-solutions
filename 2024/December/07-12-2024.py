a1413725ea40cd3a7b63799ecae69213dc926c63
Dhruva Kumar <138505718+dhr7va@users.noreply.github.com>
2024-12-07 10:42:24 +0530

    Create 07-12-2024.py
    Count Inversions
    Difficulty: MediumAccuracy: 16.93%Submissions: 578K+Points: 4
    
    Given an array of integers arr[]. Find the Inversion Count in the array.
    Two elements arr[i] and arr[j] form an inversion if arr[i] > arr[j] and i < j.
    
        Inversion Count: For an array, inversion count indicates how far (or close) the array is from being sorted. If the array is already sorted then the inversion count is 0.
        If an array is sorted in the reverse order then the inversion count is the maximum. 
    
    Examples:
    
    Input: arr[] = [2, 4, 1, 3, 5]
    Output: 3
    Explanation: The sequence 2, 4, 1, 3, 5 has three inversions (2, 1), (4, 1), (4, 3).
    
    Input: arr[] = [2, 3, 4, 5, 6]
    Output: 0
    Explanation: As the sequence is already sorted so there is no inversion count.
    
    Input: arr[] = [10, 10, 10]
    Output: 0
    Explanation: As all the elements of array are same, so there is no inversion count.
    
    Constraints:
    1 ≤ arr.size() ≤ 10^5
    1 ≤ arr[i] ≤ 10^4

---
+++ 2024/December/07-12-2024.py
@@ -0,0 +1,79 @@
+class Solution:
+
+    def inversionCount(self, arr):
+        def merge_and_count(arr, temp_arr, left, mid, right):
+            i = left    
+            j = mid + 1 
+            k = left    
+            inv_count = 0
+
+            while i <= mid and j <= right:
+                if arr[i] <= arr[j]:
+                    temp_arr[k] = arr[i]
+                    i += 1
+                else:
+                   
+                    temp_arr[k] = arr[j]
+                    inv_count += (mid - i + 1)
+                    j += 1
+                k += 1
+
+            while i <= mid:
+                temp_arr[k] = arr[i]
+                i += 1
+                k += 1
+
+            while j <= right:
+                temp_arr[k] = arr[j]
+                j += 1
+                k += 1
+
+            for i in range(left, right + 1):
+                arr[i] = temp_arr[i]
+
+            return inv_count
+
+        def merge_sort_and_count(arr, temp_arr, left, right):
+            inv_count = 0
+            if left < right:
+                mid = (left + right) // 2
+
+                inv_count += merge_sort_and_count(arr, temp_arr, left, mid)
+
+                inv_count += merge_sort_and_count(arr, temp_arr, mid + 1, right)
+
+                inv_count += merge_and_count(arr, temp_arr, left, mid, right)
+
+            return inv_count
+
+        temp_arr = [0] * len(arr)
+        return merge_sort_and_count(arr, temp_arr, 0, len(arr) - 1)
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+
+@atexit.register
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+
+if __name__ == '__main__':
+    t = int(input())
+    for tt in range(t):
+        a = list(map(int, input().strip().split()))
+        obj = Solution()
+        print(obj.inversionCount(a))
+        print("~")
+
+# } Driver Code Ends
