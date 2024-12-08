238b2f5bea923bb8299c740ad7b53951cdceea70
Dhruva Kumar <138505718+dhr7va@users.noreply.github.com>
2024-12-08 16:51:19 +0530

    Create 08-12-2024.py
    Overlapping Intervals
    Difficulty: MediumAccuracy: 57.41%Submissions: 76K+Points: 4
    
    Given an array of Intervals arr[][], where arr[i] = [starti, endi]. The task is to merge all of the overlapping Intervals.
    
    Examples:
    
    Input: arr[][] = [[1,3],[2,4],[6,8],[9,10]]
    Output: [[1,4], [6,8], [9,10]]
    Explanation: In the given intervals we have only two overlapping intervals here, [1,3] and [2,4] which on merging will become [1,4]. Therefore we will return [[1,4], [6,8], [9,10]].
    
    Input: arr[][] = [[6,8],[1,9],[2,4],[4,7]]
    Output: [[1,9]]
    Explanation: In the given intervals all the intervals overlap with the interval [1,9]. Therefore we will return [1,9].
    
    Constraints:
    1 ≤ arr.size() ≤ 10^5
    0 ≤ starti ≤ endi ≤ 10^5

---
+++ 2024/December/08-12-2024.py
@@ -0,0 +1,39 @@
+class Solution:
+	def mergeOverlap(self, arr):
+		#Code here
+        arr.sort(key=lambda x: x[0])
+
+        merged_intervals = []
+
+        for interval in arr:
+            if not merged_intervals or merged_intervals[-1][1] < interval[0]:
+                merged_intervals.append(interval)
+            else:
+                merged_intervals[-1][1] = max(merged_intervals[-1][1], interval[1])
+
+        return merged_intervals
+
+#{ 
+ # Driver Code Starts
+if __name__ == '__main__':
+    T = int(input())
+    for i in range(T):
+        n = int(input())
+        # a = list(map(int, input().strip().split()))
+        arr = []
+        # j = 0
+        for i in range(n):
+            a = list(map(int, input().strip().split()))
+            x = a[0]
+            # j += 1
+            y = a[1]
+            # j += 1
+            arr.append([x, y])
+        obj = Solution()
+        ans = obj.mergeOverlap(arr)
+        for i in ans:
+            for j in i:
+                print(j, end=" ")
+        print()
+
+# } Driver Code Ends
