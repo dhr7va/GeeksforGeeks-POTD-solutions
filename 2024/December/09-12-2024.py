0f5a5cf5591e623a04945971e87f9dee584b64e2
Dhruva Kumar <138505718+dhr7va@users.noreply.github.com>
2024-12-09 08:58:57 +0530

    Create 09-12-2024.py
    Insert Interval
    Difficulty: MediumAccuracy: 39.61%Submissions: 7K+Points: 4
    
    Geek has an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith event and intervals is sorted in ascending order by starti. He wants to add a new interval newInterval= [newStart, newEnd] where newStart and newEnd represent the start and end of this interval.
    
    Help Geek to insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
    
    Examples:
    
    Input: intervals = [[1,3], [4,5], [6,7], [8,10]], newInterval = [5,6]
    Output: [[1,3], [4,7], [8,10]]
    Explanation: The newInterval [5,6] overlaps with [4,5] and [6,7].
    
    Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,9]
    Output: [[1,2], [3,10], [12,16]]
    Explanation: The new interval [4,9] overlaps with [3,5],[6,7],[8,10].
    
    Constraints:
    1 ≤ intervals.size() ≤  10^5
    0 ≤ start[i], end[i] ≤ 10^9

---
+++ 2024/December/09-12-2024.py
@@ -0,0 +1,51 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+
+class Solution:
+    def insertInterval(self, intervals, newInterval):
+        # Code here
+        result = []
+        i = 0
+        n = len(intervals)
+
+        while i < n and intervals[i][1] < newInterval[0]:
+            result.append(intervals[i])
+            i += 1
+
+        while i < n and intervals[i][0] <= newInterval[1]:
+            newInterval[0] = min(newInterval[0], intervals[i][0])
+            newInterval[1] = max(newInterval[1], intervals[i][1])
+            i += 1
+        result.append(newInterval)
+
+        while i < n:
+            result.append(intervals[i])
+            i += 1
+
+        return result
+        
+
+#{ 
+ # Driver Code Starts.
+if __name__ == '__main__': 
+    t = int(input ())
+    for _ in range (t):
+        N = int(input())
+        intervals = [list(map(int, input().split())) for i in range(N)]
+        newEvent = list(map(int, input().split()))
+        ob = Solution()
+        res = ob.insertInterval(intervals, newEvent)
+        print('[', end = '')
+        for i in range(len(res)):
+            print('[', end = '')
+            print(str(res[i][0])+','+str(res[i][1]), end = '')
+            print(']', end = '')
+            if i < len(res)-1:
+                print(',', end='')
+        print(']')
+        print("~")
+# } Driver Code Ends
