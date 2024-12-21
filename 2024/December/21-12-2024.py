20b9212e934fd5e0f6d3777c802dc1241e95f242
Dhruva Kumar <138505718+dhr7va@users.noreply.github.com>
2024-12-21 17:08:37 +0530

    Create 21-12-2024.py
    Rotate by 90 degree
    Difficulty: EasyAccuracy: 56.88%Submissions: 95K+Points: 2
    
    Given a square matrix mat[][] of size n x n. The task is to rotate it by 90 degrees in an anti-clockwise direction without using any extra space. 
    
    Examples:
    
    Input: mat[][] = [[1, 2, 3],
                    [4, 5, 6]
                    [7, 8, 9]]
    Output: Rotated Matrix:
    [3, 6, 9]
    [2, 5, 8]
    [1, 4, 7]
    
    Input: mat[][] = [[1, 2],
                    [3, 4]]
    Output: Rotated Matrix:
    [2, 4]
    [1, 3]
    
    Constraints:
    1 ≤ n ≤ 10^2
    0 <= mat[i][j] <= 10^3

---
+++ 2024/December/21-12-2024.py
@@ -0,0 +1,48 @@
+#User function Template for python3
+
+
+class Solution:
+    
+    #Function to rotate matrix anticlockwise by 90 degrees.
+    def rotateby90(self, mat): 
+        # code here
+        n = len(mat)
+        
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                mat[i][j], mat[j][i] = mat[j][i], mat[i][j]
+        
+        for i in range(n // 2):
+            mat[i], mat[n - i - 1] = mat[n - i - 1], mat[i]
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    import sys
+    input = sys.stdin.read
+    data = input().split()
+
+    t = int(data[0])
+    index = 1
+
+    for _ in range(t):
+        n = int(data[index])
+        index += 1
+        matrix = []
+        for i in range(n):
+            row = list(map(int, data[index:index + n]))
+            matrix.append(row)
+            index += n
+        obj = Solution()
+        obj.rotateby90(matrix)
+        for i in range(n):
+            for j in range(n):
+                print(matrix[i][j], end=" ")
+            print()
+
+        print("~")
+
+# } Driver Code Ends
