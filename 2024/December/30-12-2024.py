10e9d8d0c0e0890a08f16ec66541cb0881f3bc27
Dhruva Kumar <138505718+dhr7va@users.noreply.github.com>
2024-12-30 15:40:35 +0530

    Create 30-12-2024.py
    Union of Arrays with Duplicates
    Difficulty: EasyAccuracy: 42.22%Submissions: 396K+Points: 2
    
    Given two arrays a[] and b[], the task is to find the number of elements in the union between these two arrays.
    
    The Union of the two arrays can be defined as the set containing distinct elements from both arrays. If there are repetitions, then only one element occurrence should be there in the union.
    
    Note: Elements of a[] and b[] are not necessarily distinct.
    
    Examples
    
    Input: a[] = [1, 2, 3, 4, 5], b[] = [1, 2, 3]
    Output: 5
    Explanation: Union set of both the arrays will be 1, 2, 3, 4 and 5. So count is 5.
    
    Input: a[] = [85, 25, 1, 32, 54, 6], b[] = [85, 2] 
    Output: 7
    Explanation: Union set of both the arrays will be 85, 25, 1, 32, 54, 6, and 2. So count is 7.
    
    Input: a[] = [1, 2, 1, 1, 2], b[] = [2, 2, 1, 2, 1] 
    Output: 2
    Explanation: We need to consider only distinct. So count of elements in union set will be 2.
    
    Constraints:
    1 ≤ a.size(), b.size() ≤ 10^6
    0 ≤ a[i], b[i] ≤ 10^5

---
+++ 2024/December/30-12-2024.py
@@ -0,0 +1,28 @@
+#User function Template for python3
+
+class Solution:    
+    #Function to return the count of number of elements in union of two arrays.
+    def findUnion(self, a, b):
+        # code here
+        union_set = set(a)
+        union_set.update(b)
+        
+        return len(union_set)
+        
+        
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+
+        a = [int(x) for x in input().strip().split()]
+        b = [int(x) for x in input().strip().split()]
+        ob = Solution()
+
+        print(ob.findUnion(a, b))
+        print("~")
+
+# } Driver Code Ends
