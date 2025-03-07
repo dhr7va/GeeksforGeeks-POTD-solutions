794c6be00f38ca3a4035be9c671c00a45b386d82
Dhruva Kumar <138505718+dhr7va@users.noreply.github.com>
2025-03-07 21:58:12 +0530

    Create 07-03-2025.py
    Longest Palindromic Subsequence
    Difficulty: MediumAccuracy: 56.57%Submissions: 99K+Points: 4
    
    Given a string s, return the length of the longest palindromic subsequence.
    
    A subsequence is a sequence that can be derived from the given sequence by deleting some or no elements without changing the order of the remaining elements.
    
    A palindromic sequence is a sequence that reads the same forward and backward.
    
    Examples:
    
    Input: s = "bbabcbcab"
    Output: 7
    Explanation: Subsequence "babcbab" is the longest subsequence which is also a palindrome.
    
    Input: s = "abcd"
    Output: 1
    Explanation: "a", "b", "c" and "d" are palindromic and all have a length 1.
    
    Input: s = "agbdba"
    Output: 5
    Explanation: The longest palindromic subsequence is "abdba", which has a length of 5. The characters in this subsequence are taken from the original string "agbdba", and they maintain the order of the string while forming a palindrome.
    
    Constraints:
    1 ≤ s.size() ≤ 1000
    The string contains only lowercase letters.

---
+++ 2025/March/07-03-2025.py
@@ -0,0 +1,28 @@
+#User function Template for python3
+
+class Solution:
+    def longestPalinSubseq(self, s):
+        n = len(s)
+        dp = [[0] * n for _ in range(n)]
+        for i in range(n - 1, -1, -1):
+            dp[i][i] = 1
+            for j in range(i + 1, n):
+                if s[i] == s[j]:
+                    dp[i][j] = 2 + dp[i + 1][j - 1]
+                else:
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])
+        return dp[0][n - 1]
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        s = input()
+        ob = Solution()
+        ans = ob.longestPalinSubseq(s)
+        print(ans)
+        print("~")
+# } Driver Code Ends
